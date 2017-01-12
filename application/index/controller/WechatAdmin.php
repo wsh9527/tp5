93d4ef17e5da404556f95db89beb9b96f910b087
wsh <wsh5237@126.com>
2017-01-12 15:15:07 +0800

    上传文件

--- application/index/controller/WechatAdmin.php
+++ application/index/controller/WechatAdmin.php
@@ -8,6 +8,7 @@ use think\Request;
 use think\Db;
 
 
+
 class WechatAdmin extends Controller
 {
     function _initialize()
@@ -64,25 +65,32 @@ class WechatAdmin extends Controller
     }
     function uploadFile(){
         // 获取表单上传文件 例如上传了001.jpg
-        $file = request()->file('upload');
+        $getfile = request()->file('upload');
 
-        $turepath = $file->getRealPath();
+        $turepath = $getfile->getRealPath();
         $md5path=md5_file($turepath);
         $shalpath=sha1_file($turepath);
         $map["md5"]=$md5path;
         $map["sha1"]=$shalpath;
+
         //判断文件类型
-        if(!count($file)){
+        if(!count($getfile)){
             echo '图片不能为空!';
         }
-        $type=$file->checkImg();
-        if(!$type){
+        if(!$getfile->checkImg()){
             echo '图片类型不正确!';
         }
+
         // 移动到框架应用根目录/public/uploads/ 目录下
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-        $ss = $info->getFileInfo();
-        var_dump($info);exit;
+
+
+        $ceshi = $getfile->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+dump($ceshi);exit;
+        //$info = $getfile->move(ROOT_PATH . 'public' . DS . 'uploads');
+        file_put_contents('loggggggggggggg.log',var_export($info,true),FILE_APPEND);
+        dump($info->filename);
+        exit;
         if($info){
 
         }else{
